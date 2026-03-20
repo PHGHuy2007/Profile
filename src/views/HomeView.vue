73be3d1098488b29f03e945c34db7e469f5d817f
PHGHuy <phanhoanggiahuy2007@gmail.com>
2026-03-21 03:22:34 +0700

    s

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i.ibb.co/ksW936b8/Untitled-1.png">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i.ibb.co/9mCWty4G/Untitled-2.png">
     </div>
   </main>
 </template>
